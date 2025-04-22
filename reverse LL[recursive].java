305096a32b6d2161b9fa08216ec5e2b61672be89
Rajeshkuruva2003 <rajeshkuruva3367@gmail.com>
2025-04-22 22:07:37 +0530

    Add recursive implementation of linked list reversal

---
+++ reverse LL[recursive].java
@@ -0,0 +1,12 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null|| head.next==null){
+            return head;
+        }
+       ListNode newNode=reverseList(head.next);
+       ListNode front=head.next;
+       front.next=head;
+       head.next=null;
+       return newNode;
+    }
+}
