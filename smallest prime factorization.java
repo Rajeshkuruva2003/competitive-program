9bad96a103500f818badb0ddb5d0c814949eb287
Rajeshkuruva2003 <rajeshkuruva3367@gmail.com>
2025-05-11 22:25:06 +0530

    Add implementation for smallest prime factorization of input numbers

---
+++ smallest prime factorization.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        List<Integer> ls = new ArrayList<>();
+        int n = sc.nextInt(); 
+       for (int i = 0; i < n; i++) {
+            ls.add(sc.nextInt());
+        }
+
+        int limit = 100001;
+        int[] spf = new int[limit];
+        for(int i=1;i<limit;i++){
+            spf[i]=i;
+        }
+        for(int i=2;i*i<limit;i++){
+            if(spf[i]==i){
+                for(int j=i*i;j<limit;j+=i){
+                    if(spf[j]==j){
+                        spf[j]=i;
+                    }
+                }
+            }
+        }
+         for (int num: ls){
+            while(num!=1){
+                System.out.println(spf[num]+" ");
+                num=num/spf[num];
+            }
+            System.out.println();
+        }
+    }
+}
