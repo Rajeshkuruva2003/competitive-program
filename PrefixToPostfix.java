90c66174edf2483b6bf0d3f89aca3db7fb2e45ae
Rajeshkuruva2003 <rajeshkuruva3367@gmail.com>
2025-05-22 19:42:39 +0530

    Add implementation of Prefix to Postfix conversion

---
+++ PrefixToPostfix.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class PrefixToPostfix{
+    
+    
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter prefix expression:");
+        String s=sc.nextLine();
+        Stack<String> st=new Stack<>();
+        for(int i=s.length()-1;i>=0;i--){
+            char ch=s.charAt(i);
+          
+            if (Character.isLetterOrDigit(ch)) {
+                st.push(String.valueOf(ch));  // Push operand as a string
+            } 
+            else { // operator
+                String op1 = st.pop();  // Right operand
+                String op2 = st.pop();  // Left operand
+                String expr =   op1 + op2+ ch ;
+                st.push(expr);
+            }
+        }
+       
+        
+        System.out.print(st.peek());
+        
+        
+    }
+}
