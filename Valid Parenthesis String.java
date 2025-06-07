f429c2f9f355bfec88a13b788d1abd5e1cbf2f0b
Rajeshkuruva2003 <rajeshkuruva3367@gmail.com>
2025-06-08 00:39:10 +0530

    Implement checkValidString function to validate parentheses string

---
+++ Valid Parenthesis String.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean checkValidString(String s) {
+     
+        int min=0;
+        int max=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='('){
+                min++;
+                max++;
+
+            }
+            else if(ch==')'){
+                min--;
+                max--;
+            }
+            else{
+                min=min-1;
+                max=max+1;
+            }
+            if(min<0){
+                min=0;
+            }
+            if(max<0){
+                return false;
+            }
+           
+        }
+        
+       return (min==0);
+    }
+}
+tc:O(n)
+sc:O(1)
