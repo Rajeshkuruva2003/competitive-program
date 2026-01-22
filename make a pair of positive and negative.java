fad7f736b3ab739dfc9addc838f781d75299a93e
Rajeshkuruva2003 <rajeshkuruva3367@gmail.com>
2026-01-22 22:05:12 +0530

    Add implementation for a pair of positive and negative integers

---
+++ make a pair of positive and negative.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i]>0){
+                for(int j=0;j<n;j++){
+                    if(arr[i]==-arr[j]){
+                        System.out.print(arr[i]+ " "+arr[j]);
+
+                    }
+                }
+            }
+        }
+    }
+}
